89d00270de738921217bcaa8e731e02dfa535470
Stefanus Airlangga Putra Widyantoro <st.airlangga.pw30@gmail.com>
2026-07-02 19:43:55 +0700

    Interpolation & Bindings

---
+++ part4/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import MainButton from './components/MainButton.vue'
+const greeting = 'Hello, Vue 3!'
+const blogPost = '<p style="color:red;">This is a blog post about Vue 3.</p>'
+const idBlog = 'blog1'
+</script>
+
+<template>
+  <p>{{ greeting }}</p>
+
+  <div v-bind:id="idblog">
+    {{ blogPost }}
+  </div>
+  <div v-html="blogPost"></div>
+
+  <MainButton />
+</template>
+
---
+++ part4/src/components/MainButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+const buttonDisabled = false
+const isLoading = true
+const buttonAttr = {
+    disabled: isLoading,
+    class: 'text-3xl bg-black text-white p-2 rounded-lg hover:bg-gray-800',
+}
+</script>
+
+<template>
+    <button v-bind="buttonAttr">{{ isLoading ? 'Loading' : 'Submit' }}</button>
+</template>
