98c7e383d3f3a699d47fb8f8455a4f63fe1c4714
Stefanus Airlangga Putra Widyantoro <st.airlangga.pw30@gmail.com>
2026-07-03 15:13:19 +0700

    bindings

--- part4/src/components/MainButton.vue
+++ part4/src/components/MainButton.vue
@@ -1,12 +1,24 @@
 <script setup>
-const buttonDisabled = false
-const isLoading = true
-const buttonAttr = {
-    disabled: isLoading,
+import { ref, computed } from 'vue'
+
+const isLoading = ref(false)
+
+const buttonAttr = computed(() => ({
+    disabled: isLoading.value,
     class: 'text-3xl bg-black text-white p-2 rounded-lg hover:bg-gray-800',
+}))
+
+const handleClick = () => {
+    isLoading.value = !isLoading.value
+}
+
+const renderIsLoading = () => {
+    return isLoading.value ? 'Loading' : 'Submit'
 }
 </script>
 
 <template>
-    <button v-bind="buttonAttr">{{ isLoading ? 'Loading' : 'Submit' }}</button>
+    <button v-bind="buttonAttr" @click="handleClick">
+        {{ renderIsLoading() }}
+    </button>
 </template>
